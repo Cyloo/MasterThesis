edc4f63622b0323638ee6cb4217378dcc5903097
Wangyang Shen <swycpp@gmail.com>
2016-04-25 16:56:04 +0800

    update tesseractmain.cpp

--- resources/tesseractmain.cpp
+++ resources/tesseractmain.cpp
@@ -1,22 +1,3 @@
-/**********************************************************************
-* File:        tessedit.cpp  (Formerly tessedit.c)
-* Description: Main program for merge of tess and editor.
-* Author:                  Ray Smith
-* Created:                 Tue Jan 07 15:21:46 GMT 1992
-*
-* (C) Copyright 1992, Hewlett-Packard Ltd.
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-** http://www.apache.org/licenses/LICENSE-2.0
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-*
-**********************************************************************/
-
 // Include automatically generated configuration file if running autoconf
 #ifdef HAVE_CONFIG_H
 #include "config_auto.h"
